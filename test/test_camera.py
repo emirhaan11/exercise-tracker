f5101b4d265b5d26b6deaed701fdbeb1aa64df19
emirhaan11 <emirhanyalcn11@gmail.com>
2025-08-13 23:55:22 +0300

    adding some codes

--- test/test_camera.py
+++ test/test_camera.py
@@ -1,5 +1,23 @@
 import cv2
-from src.capture import VideoCapture
+from capture import VideoCapture
 
+if __name__ == '__main__':
+    cam = VideoCapture()
 
-    
+    while True:
+        ret, frame = cam.read()
+        frame = cv2.flip(frame, 1)
+        if not ret:
+            break
+        cv2.putText(frame,
+                    f'FPS: {cam.fps:.1f}',
+                    (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2)
+        cv2.imshow('Camera', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cam.release()
+    cv2.destroyAllWindows()
